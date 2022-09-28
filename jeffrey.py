3712c572d4ac7fee9eeb1494afc2cd0f973e5774
Jeffrey Smart <jeffrey.smart@me.com>
2022-09-27 19:09:26 -0700

    Initial check-in :)

---
+++ jeffrey.py
@@ -0,0 +1,12 @@
+# todo:  write the code
+# todo:  push up to GitHub.com
+
+
+def greet(name):
+    print()
+    print(f"hello, {name}!")
+    print()
+
+
+if __name__ == "__main__":
+    greet("world")
